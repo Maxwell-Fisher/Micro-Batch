97ff1c6ee7e0dc6cbf679111c2937f2df4431fb0
Max Fisher <54481244+Maxwell-Fisher@users.noreply.github.com>
2023-02-17 01:43:47 -0700

    Oops, accidentally uploaded a testing file

--- maze - Copy.bat
+++
@@ -1,5 +0,0 @@
-﻿::Batch code tweet: a recreation of a popular program written in Commodore Basic, which draws an endless maze pattern.
-
-chcp 65001&setLocal enableDelayedExpansion&cls
-:x
-@set i=╱╲&set l!n!=!i:~%n%,1!&set /a n+=1&<%0 set /p=!l%random:~3%! &goto x
