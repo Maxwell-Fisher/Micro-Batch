3eeea8aa647d682a1d8875bd5d474e1624e181af
Maxwell Fisher <54481244+Maxwell-Fisher@users.noreply.github.com>
2023-02-17 01:42:54 -0700

    Saved a few more bytes, and made it look more like the original program I'm trying to copy

--- maze.bat
+++ maze.bat
@@ -1,6 +1,3 @@
-::Batch code tweet: a recreation of a popular program written in Basic, which draws an endless maze pattern.
-
-chcp 65001
-@setLocal enableDelayedExpansion&title Max's maze&cls
+chcp 65001&setLocal enableDelayedExpansion&cls
 :x
-@set i=┌┬┐├┼┤└┴┘│&set l!n!=!i:~%n%,1!&set /a n=n+1&echo|set /p=!l%random:~-1%!&goto x
+@set i=╱╲&set l!n!=!i:~%n%,1!&set /a n+=1&<%0 set /p=!l%random:~3%!&goto x
