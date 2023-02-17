e05a894abb405c14f54e25e9e05d3179b2386b95
Max Fisher <54481244+Maxwell-Fisher@users.noreply.github.com>
2023-02-17 02:39:16 -0700

    Saved 3 more bytes after 30 minutes

--- maze.bat
+++ maze.bat
@@ -1,3 +1,3 @@
 chcp 65001&setLocal enableDelayedExpansion&cls
 :x
-@set i=╱╲&set l!n!=!i:~%n%!&set /a n+=1&<%0 set /p=!l%random:~3%!&goto x
+@set i=╱╲&set l!n!=!i:~%n%!&set/a n+=1&<%0 set/p=!l%random:~3%!&goto x
