952d84852f7fca78ab74e20e2d34d44a2e20d2e0
Maxwell Fisher <54481244+Maxwell-Fisher@users.noreply.github.com>
2023-02-17 00:46:43 -0700

    By using @ symbols rather than disabling command output, it's possible to save 7 bytes of text

--- maze.bat
+++ maze.bat
@@ -1,6 +1,6 @@
 ::Batch code tweet: a recreation of a popular program written in Basic, which draws an endless maze pattern.
 
-echo off&chcp 65001
-setLocal enableDelayedExpansion&title Max's maze&cls
+chcp 65001
+@setLocal enableDelayedExpansion&title Max's maze&cls
 :x
-set i=┌┬┐├┼┤└┴┘│&set l!n!=!i:~%n%,1!&set /a n=n+1&echo|set /p=!l%random:~-1%!&goto x
+@set i=┌┬┐├┼┤└┴┘│&set l!n!=!i:~%n%,1!&set /a n=n+1&echo|set /p=!l%random:~-1%!&goto x
